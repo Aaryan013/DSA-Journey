6ca94481d7e32f7047c7f88efeb561fe5621794b
Aaryan Chavda <147523379+Aaryan013@users.noreply.github.com>
2026-06-25 04:04:21 +0000

    Add optimal solution for calculating left-right difference in an array

--- Array/25-06-26_2574.cpp
+++ Array/25-06-26_2574.cpp
@@ -25,3 +25,26 @@ public:
         return result;
     }
 };
+
+
+//optimal solution
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int totalSum = 0;
+
+        for (int num : nums)
+            totalSum += num;
+
+        int leftSum = 0;
+        vector<int> ans;
+
+        for (int num : nums) {
+            int rightSum = totalSum - leftSum - num;
+            ans.push_back(abs(leftSum - rightSum));
+            leftSum += num;
+        }
+
+        return ans;
+    }
+};
